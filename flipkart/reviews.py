8173e42449e38e2b7693e44e0b1ab1cda9da621b
JithinSukumaran <74790729+JithinSukumaran@users.noreply.github.com>
2022-03-09 22:57:37 +0530

    Update reviews.py

--- flipkart/reviews.py
+++ flipkart/reviews.py
@@ -4,13 +4,11 @@ from scrapy import Request
 class ReviewsSpider(scrapy.Spider):
     name = 'reviews'
     allowed_domains = ['flipkart.com']
-#    start_urls = [url]
-    # start_urls = ['https://www.flipkart.com/apple-iphone-12-mini-black-64-gb/product-reviews/itm38b727191eb08?pid=MOBFWBYZXSEGBS6F&lid=LSTMOBFWBYZXSEGBS6FTONJ6W&marketplace=FLIPKART&page=549']
+    start_urls = ['https://www.flipkart.com/apple-iphone-12-mini-black-64-gb/product-reviews/itm38b727191eb08?pid=MOBFWBYZXSEGBS6F&lid=LSTMOBFWBYZXSEGBS6FTONJ6W&marketplace=FLIPKART&page=549']
     count = 0
 
     def parse(self, response):
         reviews = response.xpath('//div[@class="col _2wzgFH K0kLPL"]')
-
         for review in reviews:
             data ={
             'Stars' : review.xpath('.//div[@class="row"]/div/text()').extract_first(),
@@ -23,17 +21,10 @@ class ReviewsSpider(scrapy.Spider):
             'Likes of other users' : review.xpath('.//span[@class="_3c3Px5"]/text()').extract()[0],
             'dislikes of other users' : review.xpath('.//span[@class="_3c3Px5"]/text()').extract()[1]
             }
-
             yield data
 
         if response.xpath('//a[@class="_1LKTO3"]/span/text()').extract()[-1] == 'Next':
-            # self.count +=1
             next_url = response.xpath('//a[@class="_1LKTO3"]/@href').extract()[-1]
             absolute_next_url = response.urljoin(next_url)
             yield Request(absolute_next_url,callback=self.parse,dont_filter=True)
 
-# class for 2 star
-# class="_3LWZlK _32lA32 _1BLPMq"
-
-# class for 1 star
-# class="_3LWZlK _1rdVr6 _1BLPMq"
